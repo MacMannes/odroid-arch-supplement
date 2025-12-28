d77fbc85e7af49a1a688f771185513d35042957b
Andre Mathlener <info@macmannes.nl>
2025-12-28 16:53:41 +0100

    fix: install yay

--- install-yay.sh
+++ install-yay.sh
@@ -6,7 +6,13 @@ source ./functions.sh
 if which yay >/dev/null 2>&1; then
     msg "yay already installed"
 else
-    run sudo pacman -S --needed --noconfirm yay
+    run sudo pacman -S --needed base-devel git
+    run sudo pacman -S --needed git base-devel
+    run pushd ~
+    run git clone https://aur.archlinux.org/yay.git
+    run cd yay
+    run makepkg -si
+    run popd
 fi
 
 done_step
